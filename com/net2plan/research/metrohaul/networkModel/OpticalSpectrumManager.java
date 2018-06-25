acf7dfcfe334b45b9929f9c6a0d2b7f679ce0d17
Pablo Pavón Mariño <pablo.pavon@upct.es>
2018-06-25 10:05:13 +0200

    WIP

--- com/net2plan/research/metrohaul/networkModel/OpticalSpectrumManager.java
+++ com/net2plan/research/metrohaul/networkModel/OpticalSpectrumManager.java
@@ -15,6 +15,10 @@ public class OpticalSpectrumManager
 
 	private OpticalSpectrumManager () {}
 	
+	/** Creates this object, asociated to a given network
+	 * @param net the network
+	 * @return see above
+	 */
 	public static OpticalSpectrumManager createFromRegularLps (WNet net)
     {
 		final OpticalSpectrumManager osm = new OpticalSpectrumManager();
@@ -23,6 +27,10 @@ public class OpticalSpectrumManager
         return osm;
     }
 
+    /** Returns the set of the optical slots ids that are idle in ALL the fibers provided 
+     * @param wdmLinks the set of fibers
+     * @return see above
+     */
     public SortedSet<Integer> getAvailableSlotIds (Collection<WFiber> wdmLinks)
     {
         if (wdmLinks.isEmpty()) throw new Net2PlanException ("No WDM links");
@@ -34,16 +42,29 @@ public class OpticalSpectrumManager
         return validSlotIds;
     }
     
+    /** Given a lightpath request, returns the optical slots occupied in the traversed fibers
+     * @param lp the lightpath request
+     * @return see above
+     */
     public SortedMap<WFiber,SortedSet<Integer>> getOccupiedResources (WLightpathRequest lp)
     {
     	return this.occupation_ll_f_s.getOrDefault(lp, new TreeMap<> ());
     }
 
+    /** Given a fiber, returns a map with the occupied optical slot ids, mapped to the set of lightpaths that occupy it. 
+     * Note that if more than one lightpath occupies a given slot, means that spectrum clashing occurs in that slot   
+     * @param fiber the input fiber
+     * @return see above
+     */
     public SortedMap<Integer,SortedSet<WLightpathUnregenerated>> getOccupiedResources (WFiber fiber)
     {
     	return this.occupation_f_s_ll.getOrDefault(fiber, new TreeMap<> ());
     }
 
+    /** Given a fiber, returns the set of optical slots occupied by at least one traversing lightpath
+     * @param fiber see above
+     * @return see above
+     */
     public SortedSet<Integer> getOccupiedOpticalSlotIds (WFiber fiber)
     {
 		SortedMap<Integer,SortedSet<WLightpathUnregenerated>> occupiedSlotsPerLightpath = this.occupation_f_s_ll.get(fiber);
@@ -52,6 +73,11 @@ public class OpticalSpectrumManager
     	return new TreeSet<> (occupiedSlotsPerLightpath.keySet());
     }
 
+    /** Given a set of fibers and a set of optical slots, returns true if ALL the optical slots are idle in ALL the fibers 
+     * @param wdmLinks the fibers
+     * @param slotIds the optical slots
+     * @return see above
+     */
     public boolean isAllocatable (Collection<WFiber> wdmLinks , SortedSet<Integer> slotIds)
     {
         if (wdmLinks.size() != new HashSet<> (wdmLinks).size()) return false;
@@ -61,8 +87,22 @@ public class OpticalSpectrumManager
         return true;
     }
 
+    /** Returns true if the indicated lightpath is already accounted in this optical spectrm manager. This means that its occupation 
+     * of optical slots in the traversed fibers is accounted for. Note that if the lightpath ocupation was introduced, and then 
+     * its route is changed, the occupied resources should be updated by the user in optical spectrum manager.  
+     * @param lp the lightpath
+     * @return see above
+     */
     public boolean isAlreadyAccounted (WLightpathUnregenerated lp) { return this.occupation_ll_f_s.get(lp) != null; }
 
+    /** Accounts for the occupation of a lightpath, updating the information in the spectrum manager
+     * @param lp the lightpath
+     * @param wdmLinks the set of fibers where optical resources are occupied by this lightpath. This is typically the set of 
+     * lightpath traversed fibers. In filterless technologies, this may also include other fibers not intentionally traversed, 
+     * but where the spectrum is also occupied
+     * @param slotIds the optical slot ids
+     * @return see above
+     */
     public boolean allocateOccupation (WLightpathUnregenerated lp , Collection<WFiber> wdmLinks , SortedSet<Integer> slotIds)
     {
     	if (isAlreadyAccounted(lp)) throw new Net2PlanException ("The lightpath has been already accounted for");
@@ -84,6 +124,9 @@ public class OpticalSpectrumManager
     	return clashesWithPreviousAllocations;
     }
 
+    /** Releases all the optical slots occupied for a given lightpath in this manager
+     * @param lp the lightpath
+     */
     public void releaseOccupation (WLightpathUnregenerated lp)
     {
     	final SortedMap<WFiber,SortedSet<Integer>> occupiedResources = occupation_ll_f_s.get(lp);
@@ -112,10 +155,23 @@ public class OpticalSpectrumManager
     }
 
     
+    /** Searches for a first-fit assignment. Given a set of fibers to occupy, the number of contiguous optical slots needed, 
+     * and (optionally) an initial optical slot (so optical slots of lower id are not consiedered), this method searches for 
+     * the lowest-id contiguous range of slots that are available in all the indicated fibers. Note that if the set of fibers 
+     * passes more than once in the same fiber, no assignment is possible, and Optional.empty is returned
+     * @param seqFibers see above
+     * @param numContiguousSlotsRequired see above
+     * @param minimumInitialSlotId see above
+     * @return see above. If no idle range is found, Optional.empty is returned. Note that only the first slot is returned, but 
+     * availability is guaranteed for all the contigous set of slots.
+     */
     public Optional<Integer> spectrumAssignment_firstFit(Collection<WFiber> seqFibers, int numContiguousSlotsRequired , Optional<Integer> minimumInitialSlotId)
     {
         assert !seqFibers.isEmpty();
         assert numContiguousSlotsRequired > 0;
+        
+        /* If a fiber is traversed more than once, there is no possible assignment */
+        if (!(seqFibers instanceof Set)) if (new HashSet<> (seqFibers).size() != seqFibers.size()) return Optional.empty();
         SortedSet<Integer> intersectionValidSlots = getAvailableSlotIds(seqFibers);
         if (minimumInitialSlotId.isPresent())
             intersectionValidSlots = intersectionValidSlots.tailSet(minimumInitialSlotId.get());
@@ -134,8 +190,22 @@ public class OpticalSpectrumManager
         return Optional.empty();
     }
 
+    /** Searches for a first-fit assignment for the two given paths, so optical slots can be different for each. 
+     * Given two sets of fibers to occupy (paths), the number of contiguous optical slots needed in each, 
+     * this method searches for the two lowest-id contiguous ranges of slots, so the first range is available in the first path,
+     * the second range is available in the second path. Note that if any path contains a fiber more than once, no allocation is 
+     * possible. Note that if path1 and path2 have common fibers, the optical slots returned will always be disjoint.
+     * @param seqFibers_1 see above
+     * @param seqFibers_2 see above
+     * @param numContiguousSlotsRequired see above
+     * @return see above. If no idle range is found, Optional.empty is returned. Note that only the first slot is returned, but 
+     * availability is guaranteed for all the contigous set of slots.
+     */
     public Optional<Pair<Integer,Integer>> spectrumAssignment_firstFitTwoRoutes(Collection<WFiber> seqFibers_1, Collection<WFiber> seqFibers_2 , int numContiguousSlotsRequired)
     {
+        /* If a fiber is traversed more than once in any path, there is no possible assignment */
+        if (!(seqFibers_1 instanceof Set)) if (new HashSet<> (seqFibers_1).size() != seqFibers_1.size()) return Optional.empty();
+        if (!(seqFibers_2 instanceof Set)) if (new HashSet<> (seqFibers_2).size() != seqFibers_2.size()) return Optional.empty();
         final boolean haveLinksInCommon = !Sets.intersection(new HashSet<>(seqFibers_1)  , new HashSet<>(seqFibers_2)).isEmpty();
         if (!haveLinksInCommon)
         {
@@ -162,7 +232,15 @@ public class OpticalSpectrumManager
         return Optional.empty();
     }
 
-    public List<List<WFiber>> getRegenerationPoints (List<WFiber> seqFibers, double maxRegeneratorDistanceInKm)
+    /** Given a sequence of fibers (i.e. an intended path of a lightpath) and the maximum unreqenerated distance applicable 
+     * for it (maximum km that can traverse without OEO regeneration), returns a partitioning of the input sequence of fibers 
+     * into one or more OCh paths (List<WFiber), so that each OCh path has a length lower or equal than the maxUnregeneratedDistanceInKm, 
+     * and the number of OChs is minimized. 
+     * @param seqFibers see above
+     * @param maxUnregeneratedDistanceInKm  see above 
+     * @return  see above
+     */
+    public List<List<WFiber>> getRegenerationPoints (List<WFiber> seqFibers, double maxUnregeneratedDistanceInKm)
     {
         final List<List<WFiber>> res = new ArrayList<> ();
         res.add(new ArrayList<> ());
@@ -171,10 +249,10 @@ public class OpticalSpectrumManager
         for (WFiber fiber : seqFibers)
         {
             final double fiberLengthInKm = fiber.getLengthInKm();
-            if (fiberLengthInKm > maxRegeneratorDistanceInKm)
-                throw new Net2PlanException(String.format("Fiber %d is longer (%f km) than the maximum distance without regenerators (%f km)", fiber.getId(), fiberLengthInKm, maxRegeneratorDistanceInKm));
+            if (fiberLengthInKm > maxUnregeneratedDistanceInKm)
+                throw new Net2PlanException(String.format("Fiber %d is longer (%f km) than the maximum distance without regenerators (%f km)", fiber.getId(), fiberLengthInKm, maxUnregeneratedDistanceInKm));
             accumDistance += fiberLengthInKm;
-            if (accumDistance > maxRegeneratorDistanceInKm)
+            if (accumDistance > maxUnregeneratedDistanceInKm)
             {
                 res.add(new ArrayList<> (Arrays.asList(fiber)));
                 accumDistance = fiberLengthInKm;
@@ -186,6 +264,9 @@ public class OpticalSpectrumManager
         return res;
     }
 
+    /** Prints a report with the occupation (for debugging pruposes)
+     * @return see above
+     */
     public String printReport ()
     {
         final StringBuffer st = new StringBuffer ();
@@ -202,6 +283,11 @@ public class OpticalSpectrumManager
         return st.toString();
     }
 
+    PABLO: HASWAVELENGTH CLAHING? CHECKS
+    
+    /** Checks if the optical slot occupation --
+     * 
+     */
     public void checkNetworkSlotOccupation ()
     {
         for (Entry<WFiber,SortedMap<Integer,SortedSet<WLightpathUnregenerated>>> occup_e : occupation_f_s_ll.entrySet())
@@ -224,6 +310,10 @@ public class OpticalSpectrumManager
         return true;
     }
 
+	/** Returns the optical slots that are usable (valid and idle) in the given fiber
+	 * @param wdmLink see above
+	 * @return  see above
+	 */
 	public SortedSet<Integer> getIdleOpticalSlotIds (WFiber wdmLink)
 	{
 		final SortedSet<Integer> res = wdmLink.getValidOpticalSlotIds();
@@ -231,6 +321,11 @@ public class OpticalSpectrumManager
 		return res;
 	}
 	
+	/** Indicates if the optical slots are usable (valid and idle) in the given fiber
+	 * @param wdmLink see above
+	 * @param slotsIds see above
+	 * @return see above
+	 */
 	public boolean isOpticalSlotIdsValidAndIdle (WFiber wdmLink , SortedSet<Integer> slotsIds)
 	{
 		return getIdleOpticalSlotIds(wdmLink).containsAll(slotsIds);
